41f80c0b413c9310e60778f49b5d705fe2b08bc2
yflin <yflin@bu.edu>
2020-01-25 16:39:12 -0500

    updated assert

--- arabic2roman.py
+++ arabic2roman.py
@@ -2,7 +2,12 @@
 #EC500:Assignment one
 #Arabic to Roman Numerals
 
+
 def toRoman(n):
+	assert 0 < n < 3999, "Input out of range"
+	assert isinstance(n,int), "Incorrect input type"
+    #can't represent below 1 and above 3999
+    #n should be an integer
 	res = ''
 	nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 	romans =  ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
@@ -18,3 +23,4 @@ def toRoman(n):
 if __name__ == "__main__":
 	print(toRoman(988))
 	print(toRoman(1340))
+	print(toRoman(4000))
