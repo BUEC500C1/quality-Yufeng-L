7d464042505674e8eba7104cf3806d46efff6f8b
yflin <yflin@bu.edu>
2020-01-25 21:58:18 -0500

    updated

--- arabic2roman.py
+++ arabic2roman.py
@@ -4,23 +4,20 @@
 
 
 def toRoman(n):
-	assert 0 < n < 3999, "Input out of range"
-	assert isinstance(n,int), "Incorrect input type"
-    #can't represent below 1 and above 3999
-    #n should be an integer
+    #can't represent integer below 1 and above 3999
 	res = ''
 	nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-	romans =  ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-	
+	romans =  ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]	
 	temp = n
-
-	for i in range(len(nums)):
-		while temp >= nums[i]:
-			temp -= nums[i]
-			res += romans[i]
-	return res
-
+	if 0 < temp < 3999 and isinstance(temp,int):
+		for i in range(len(nums)):
+		    while temp >= nums[i]:
+			    temp -= nums[i]
+			    res += romans[i]
+		return res
+	else:
+	    return -1
 if __name__ == "__main__":
-	testset = [5,18,29,1350,3999]
-	for element in testset:
-		print(toRoman(element))
+	testset = [3,6,18,101,-1,0,3999,1340]
+	for ele in testset:
+		print(toRoman(ele))
