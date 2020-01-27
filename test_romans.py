3c0912ea32ef85c48175d9ba6786d5037cddedac
yflin <yflin@bu.edu>
2020-01-27 16:39:07 -0500

    updated_test

--- test_romans.py
+++ test_romans.py
@@ -5,13 +5,23 @@ import pytest
 import arabic2roman
 
 
-def testans():
+def testset1():
 
 	arabic = [3,6,18,101,-1,0,3999,1340,4001]
 	correct_ans =["III","VI","XVIII","CI",-1,-1,-1,"MCCCXL",-1]
 	for a,c in zip(arabic,correct_ans):
 		assert arabic2roman.toRoman(a) == c
 
+
+def testset2():
+	assert arabic2roman.toRoman(1) == "I"
+	assert arabic2roman.toRoman(2) == "II"
+	assert arabic2roman.toRoman(4) == "IV"
+	assert arabic2roman.toRoman(999) == "CMXCIX"
+	assert arabic2roman.toRoman(1024) == "MXXIV"
+	assert arabic2roman.toRoman(-3) == -1
+	assert arabic2roman.toRoman(-267) == -1
+	assert arabic2roman.toRoman(6789) == -1
 
 # if __name__ == "__main__":
 # 	testans()
